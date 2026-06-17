d9ac95b0262e0e69a617026a8336f3419a0bf7a3
Mohan  A L <almohan0728@gmail.com>
2026-06-17 13:45:08 +0530

    Time: 1 ms (100.00%) | Memory: 48.9 MB (51.11%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,46 +1,32 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        if(nums1.length > nums2.length) return findMedianSortedArrays(nums2, nums1);
+
         int m = nums1.length;
         int n = nums2.length;
 
-        int total = m + n;
+        int low = 0;
+        int high = m;
 
-        int ind1 = (total - 1) / 2;
-        int ind2 = total / 2;
+        while(low <= high) {
+            int cut1 = low + (high - low)/2;
+            int cut2 = (m + n + 1) / 2 - cut1;
 
-        int i=0, j=0, cnt = 0;
-        int ele1 = 0, ele2 = 0;
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE:nums1[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE:nums2[cut2 - 1];
 
-        while(i < m && j < n) {
-            int curr;
-            if(nums1[i] <= nums2[j]) curr = nums1[i++];
-            else curr = nums2[j++];
+            int r1 = (cut1 == m) ? Integer.MAX_VALUE:nums1[cut1];
+            int r2 = (cut2 == n) ? Integer.MAX_VALUE:nums2[cut2];
 
-            if(cnt == ind1) ele1 = curr;
-            if(cnt == ind2) ele2 = curr;
-
-            cnt++;
+            if(l1 <= r2 && l2 <= r1) {
+                if((m+n) % 2 ==0) {
+                    return (Math.max(l1,l2) + Math.min(r1, r2)) / 2.0; 
+                }
+                return Math.max(l1, l2);
+            }
+            else if(l1 > r2) high = cut1 - 1;
+            else low = cut1 + 1;
         }
-        while(i < m) {
-            int curr = nums1[i++];
-
-            if(cnt == ind1) ele1 = curr;
-            if(cnt == ind2) ele2 = curr;
-
-            cnt++;
-        }
-
-        while(j < n) {
-            int curr = nums2[j++];
-
-            if(cnt == ind1) ele1 = curr;
-            if(cnt == ind2) ele2 = curr;
-
-            cnt++;
-        }
-
-        if(total % 2 == 1) return ele2;
-
-        return (ele1 + ele2) / 2.0;
+        return 0;
     }
 }
