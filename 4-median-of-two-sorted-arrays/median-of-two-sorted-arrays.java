b93d5c7e56e8d945cd577dd789877790e4853790
Mohan  A L <almohan0728@gmail.com>
2026-06-17 12:51:25 +0530

    Time: 2 ms (45.78%) | Memory: 49 MB (31.74%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -3,21 +3,44 @@ class Solution {
         int m = nums1.length;
         int n = nums2.length;
 
-        int[] arr = new int[m+n];
-        int i=0, j=0, k=0;
+        int total = m + n;
+
+        int ind1 = (total - 1) / 2;
+        int ind2 = total / 2;
+
+        int i=0, j=0, cnt = 0;
+        int ele1 = 0, ele2 = 0;
 
         while(i < m && j < n) {
-            if(nums1[i] <= nums2[j]) arr[k++] = nums1[i++];
-            else arr[k++] = nums2[j++];
+            int curr;
+            if(nums1[i] <= nums2[j]) curr = nums1[i++];
+            else curr = nums2[j++];
+
+            if(cnt == ind1) ele1 = curr;
+            if(cnt == ind2) ele2 = curr;
+
+            cnt++;
+        }
+        while(i < m) {
+            int curr = nums1[i++];
+
+            if(cnt == ind1) ele1 = curr;
+            if(cnt == ind2) ele2 = curr;
+
+            cnt++;
         }
 
-        while(i < m) arr[k++] = nums1[i++];
-        while(j < n) arr[k++] = nums2[j++];
+        while(j < n) {
+            int curr = nums2[j++];
 
-        int len = m + n;
+            if(cnt == ind1) ele1 = curr;
+            if(cnt == ind2) ele2 = curr;
 
-        if(len % 2 == 1) return arr[len / 2];
+            cnt++;
+        }
 
-        return (arr[len / 2] + arr[(len / 2) - 1 ]) / 2.0;
+        if(total % 2 == 1) return ele2;
+
+        return (ele1 + ele2) / 2.0;
     }
 }
