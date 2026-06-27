99a4b0e93e91d5187aa5a0426a4e6ac415d1bb03
Mohan  A L <almohan0728@gmail.com>
2026-06-27 13:33:32 +0530

    Time: 4 ms (5.61%), Space: 47 MB (8.81%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        LinkedHashSet<Integer> set = new LinkedHashSet<>();
+
+        for(int num: nums) {
+            set.add(num);
+        }
+
+        int index = 0;
+        for(int num: set) {
+            nums[index++] = num;
+        }
+
+        return set.size();
+    }
+}
