d3acd952c73b90b2e0e2ade259e15ce215e03cc7
Mohan  A L <almohan0728@gmail.com>
2026-07-05 18:55:58 +0530

    Time: 0 ms (100%), Space: 47.6 MB (85.68%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+
+        if(N < 0) {
+            x = 1/x;
+            N = -N;
+        }
+
+        double pow = 1.0;
+
+        while(N > 0) {
+            if((N & 1) == 1) {
+                pow *= x;
+            }
+            x *= x;
+            N >>>=1;
+        }
+        return pow;
+
+    }
+}
