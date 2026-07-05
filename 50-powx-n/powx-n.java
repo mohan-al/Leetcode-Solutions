9fcc188de64bf60d21adcbcb627ec92513340963
Mohan  A L <almohan0728@gmail.com>
2026-07-05 18:54:03 +0530

    Time: 0 ms (100.00%) | Memory: 48.3 MB (22.33%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,22 @@
+class Solution {
+    public double myPow(double x, int n) {
+        long N = n;
+
+        if(N < 0) {
+            x = 1/x;
+            N = -N;
+        }
+
+        double pow = 1.0;
+
+        while(N > 0) {
+            if((N % 2) == 1) {
+                pow *= x;
+            }
+            x *= x;
+            N = N/2;
+        }
+        return pow;
+
+    }
+}
