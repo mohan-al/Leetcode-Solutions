daf415ad68102418f7ec5264ee844338553c47df
Mohan  A L <almohan0728@gmail.com>
2026-07-18 23:09:14 +0530

    Time: 4 ms (62%), Space: 44.3 MB (40.8%) - LeetHub

---
+++ 0987-vertical-order-traversal-of-a-binary-tree/0987-vertical-order-traversal-of-a-binary-tree.java
@@ -0,0 +1,65 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    class Pair {
+        TreeNode node;
+        int row;
+        int col;
+
+        Pair(TreeNode node, int row, int col) {
+            this.node = node;
+            this.row = row;
+            this.col = col;
+        }
+    }
+    public List<List<Integer>> verticalTraversal(TreeNode root) {
+        TreeMap<Integer, TreeMap<Integer, PriorityQueue<Integer>>> map = new TreeMap<>();
+        Queue<Pair> queue = new LinkedList<>();
+        queue.offer(new Pair(root, 0, 0));
+
+        while(!queue.isEmpty()) {
+            Pair curr = queue.poll();
+            
+            TreeNode node = curr.node;
+            int row = curr.row;
+            int col = curr.col;
+
+            map.putIfAbsent(col, new TreeMap<>());
+            map.get(col).putIfAbsent(row, new PriorityQueue<>());
+            map.get(col).get(row).offer(node.val);
+
+            if(node.left != null) {
+                queue.offer(new Pair(node.left, row+1, col - 1));
+            }
+              if(node.right != null) {
+                queue.offer(new Pair(node.right, row+1, col + 1));
+            }
+        }
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(TreeMap<Integer, PriorityQueue<Integer>> rows : map.values()) {
+            List<Integer> list = new ArrayList<>();
+
+            for(PriorityQueue<Integer> pq: rows.values()) {
+                while(!pq.isEmpty()) {
+                    list.add(pq.poll());
+                }
+            }
+            ans.add(list);
+        }
+        return ans;
+    }
+}
