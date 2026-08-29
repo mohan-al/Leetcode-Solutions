1f849659af18a8e43e9e099844924f222fb554fc
Mohan  A L <almohan0728@gmail.com>
2026-08-29 11:49:56 +0530

    Time: 0 ms (100.00%) | Memory: 43.2 MB (91.73%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -3,24 +3,19 @@ class Solution {
         int m = a.length;
         int n = a[0].length;
 
-        int[][] dp = new int[m][n];
-        for(int[] row: dp) {
-            Arrays.fill(row, -1);
+        int[] dp = new int[n];
+        dp[0] = 1;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(a[i][j] == 1) {
+                    dp[j] = 0;
+                }
+                else if(j > 0) {
+                    dp[j] = dp[j] + dp[j-1];
+                }
+            }
         }
-
-        return solve(0, 0, a, dp, m, n);
-    }
-    private int solve(int i, int j, int[][] a, int[][] dp, int m, int n) {
-        if(i >= m || j >= n) return 0;
-        if(a[i][j] == 1) return 0;
-
-        if(i == m-1 && j == n-1) return 1;
-        if(dp[i][j] != -1) return dp[i][j];
-
-        int down = solve(i+1, j, a, dp, m, n);
-        int right = solve(i, j+1, a, dp, m, n);
-
-        dp[i][j] = down + right;
-        return dp[i][j];
+        return dp[n-1];
     }
 }
