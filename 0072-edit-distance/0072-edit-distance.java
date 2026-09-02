04ec166342d7594fb58906a7b21e4423c808c089
Mohan  A L <almohan0728@gmail.com>
2026-09-02 23:14:53 +0530

    Time: 5 ms (65.77%), Space: 47.5 MB (5.89%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -3,30 +3,30 @@ class Solution {
         int m = word1.length();
         int n = word2.length();
 
-        int[][] dp = new int[m][n];
-        for(int[] row: dp) {
-            Arrays.fill(row, -1);
+        int[][] dp = new int[m+1][n+1];
+
+        for(int j=0; j<=n; j++) {
+            dp[0][j] = j;
         }
 
-        return solve(m-1, n-1, word1, word2, dp);
-    }
-    private int solve(int i, int j, String word1, String word2, int[][] dp) {
-        if(i < 0) return j+1;
-        if(j < 0) return i+1;
-
-        if(dp[i][j] != -1) return dp[i][j];
-
-        if(word1.charAt(i) == word2.charAt(j)) {
-            dp[i][j] = solve(i-1, j-1, word1, word2, dp);
+         for(int i=0; i<=m; i++) {
+            dp[i][0] = i;
         }
 
-        else {
-             int insert = solve(i, j-1, word1, word2, dp);
-             int delete = solve(i-1, j, word1, word2, dp);
-             int replace = solve(i-1, j-1, word1, word2, dp);
+        for(int i=1; i<=m; i++) {
+            for(int j=1; j<=n; j++) {
+                if(word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else {
+                    int replace = dp[i-1][j-1];
+                    int insert = dp[i][j-1];
+                    int delete = dp[i-1][j];
 
-             dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
-       }
-       return dp[i][j];
+                    dp[i][j] = 1 + Math.min(insert, Math.min(delete, replace));
+                }
+            }
+        }
+        return dp[m][n];
     }
 }
