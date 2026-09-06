f425d3835d8b003c093a5ed65b5b27d893cfeb1f
Mohan  A L <almohan0728@gmail.com>
2026-09-06 11:37:46 +0530

    Time: 13 ms (74.27%) | Memory: 46.2 MB (93.84%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.java
@@ -1,31 +1,22 @@
 class Solution {
     public String frequencySort(String s) {
-        
         HashMap<Character, Integer> map = new HashMap<>();
 
         for(char ch: s.toCharArray()) {
-            map.put(ch, map.getOrDefault(ch, 0) +1);
+            map.put(ch, map.getOrDefault(ch, 0) + 1);
         }
 
-        List<Character>[] bucket = new ArrayList[s.length() + 1];
-
-        for(char ch: map.keySet()) {
-
-            int freq = map.get(ch);
-            if(bucket[freq] == null) bucket[freq] = new ArrayList<>();
-
-            bucket[freq].add(ch);
-        }
+        PriorityQueue<Character> pq = new PriorityQueue<>((a, b) -> map.get(b) - map.get(a));
+        pq.addAll(map.keySet());
 
         StringBuilder ans = new StringBuilder();
 
-        for(int freq = s.length(); freq >= 1; freq--) {
-            if(bucket[freq] != null) {
-                for(char ch: bucket[freq]) {
-                    for(int i=0;i<freq;i++) {
-                        ans.append(ch);
-                    }
-                }
+        while(!pq.isEmpty()) {
+            char ch = pq.poll();
+            int freq = map.get(ch);
+
+            for(int i=0; i<freq; i++) {
+                ans.append(ch);
             }
         }
         return ans.toString();
